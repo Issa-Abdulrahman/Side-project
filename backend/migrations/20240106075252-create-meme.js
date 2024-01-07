8443e24095a8ffeb0f37a0d2123f1a0707447329
Issa <isaiahabdulrahman@gmail.com>
2024-01-07 15:07:44 +0200

    I added the userId to the meme

---
+++ backend/migrations/20240106075252-create-meme.js
@@ -0,0 +1,33 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Memes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      caption: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Memes');
+  }
+};
