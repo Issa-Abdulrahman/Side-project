b316dcf2dfaaad8d44f0dd60a302f9aacfb36805
Issa <isaiahabdulrahman@gmail.com>
2024-01-08 00:41:50 +0200

    index file in the models and associations are done

--- backend/models/meme.js
+++ backend/models/meme.js
@@ -4,13 +4,13 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Meme extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Meme.belongsTo(models.User, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      })
+      
     }
   }
   Meme.init({
