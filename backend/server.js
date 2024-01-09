25f345614a2328e9120fb8f64ed71f5ae5e529ac
Issa <isaiahabdulrahman@gmail.com>
2024-01-10 01:56:49 +0200

    server and some files correction are done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import { Express } from "express";
 import db from "./models/index.js";
 import memesRoutes from "./routes/MemesRoutes.js"
-
+import cookieParser from "cookie-parser";
+import authRoutes from"./routes/AuthRoutes.js";
+import cors from "cors";
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -9,7 +11,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 app.use('/meme', memesRoutes);
+app.use('/auth', authRoutes);
 
 app.listen( port, async() => {
     console.log(`server is running  on port ${port}`)
