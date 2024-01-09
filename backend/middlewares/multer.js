5be12089b6b1f23cd02c95d3ee7af9ff9fd1bd8b
Issa <isaiahabdulrahman@gmail.com>
2024-01-10 00:27:58 +0200

    I did multer file in the middlewares

---
+++ backend/middlewares/multer.js
@@ -0,0 +1,25 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "images");
+    },
+    filename:(req, file, callback) => {
+        callback(null,"image" + Date.now() + path.extname(file.originalname));
+    },
+}); 
+const fileFilter = (req, file, callback) => {
+    const allowedFileTypes = /jpeg|jpg|png/;
+    const extname = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedFileTypes.test(file.mimetype);
+    
+    if (extname && mimetype) {
+      callback(null, true);
+    } else {
+      callback(new Error('Only images are allowed (jpeg, jpg, png)'));
+    }
+  };
+
+  const uploadImage= multer({ storage: storage});
+  export default uploadImage;
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import { Express } from "express";
+import db from "./models/index.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen( port, async() => {
+    console.log(`server is running  on port ${port}`)
+    try {
+        await db.sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+        await db.sequelize.sync();
+        console.log('Database synced!');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+})
